abbad7cc35c85e9f2a981760eeb03e0b07484df0
frognar <sebastian.przyszlak@gmail.com>
2024-10-05 11:25:26 +0200

    Introduce Name type and utility methods for creating and validating names

---
+++ src/BookCatalog.Domain/Models/Books/NameType.cs
@@ -0,0 +1,27 @@
+using DotMaybe;
+
+namespace BookCatalog.Domain.Models.Books;
+
+public abstract record NameType;
+
+internal record FullNameType(string FirstName, string LastName) : NameType;
+
+internal record MononymType(string Name) : NameType;
+
+public static class Name
+{
+    public static Maybe<NameType> Create(string firstName, string lastName)
+        => string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName)
+            ? None.OfType<NameType>()
+            : Some.With<NameType>(new FullNameType(firstName.Trim(), lastName.Trim()));
+
+    public static Maybe<NameType> Create(string name)
+        => string.IsNullOrWhiteSpace(name)
+            ? None.OfType<NameType>()
+            : Some.With<NameType>(new MononymType(name.Trim()));
+
+    public static Maybe<IEnumerable<NameType>> CreateMany(params Maybe<NameType>[] names)
+        => names.Aggregate(
+            Some.With<IEnumerable<NameType>>(new List<NameType>()),
+            (current, name) => Maybe.Map2(current, name, (c, n) => c.Append(n)));
+}
